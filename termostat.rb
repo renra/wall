ae9cd220a70706ebcb2b66828e361da6ad8bf450
frantisek.gloser@gmail.com <frantisek.gloser@gmail.com>
2016-07-21 10:37:24 +0200

    formatting change in termostat.rb

--- termostat.rb
+++ termostat.rb
@@ -1,34 +1,33 @@
 class Termostat
 
   def check_temperature
-  	# define input of measured temperature
+    # define input of measured temperature
   end
 
   def start_heating
-  	# define output for heating on
+    # define output for heating on
   end
 
   def stop_heating
-  	# define output for heating off
+    # define output for heating off
   end
 
-	def operation(set_temperature)
+  def operation(set_temperature)
     @temperature = check_temperature
-		@set_temperature = set_temperature
-		if @temperature >= @set_temperature heating
-		else stop_heating
-		end
-	end
+    @set_temperature = set_temperature
+    if @temperature >= @set_temperature heating
+      else stop_heating
+    end
+  end
 
-	def set_temperature_up(about)
-		@set_temperature += about
-	end
+  def set_temperature_up(about)
+    @set_temperature += about
+  end
 
-	def set_temperature_down(about)
-		@set_temperature -= about
-	end
+  def set_temperature_down(about)
+    @set_temperature -= about
+  end
 	
-
 end
 
 
