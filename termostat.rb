42269dd9b9ef03ef6edd3aa60c227bb840e72a9d
frantisek.gloser@gmail.com <frantisek.gloser@gmail.com>
2016-07-22 14:16:33 +0200

    termostat.rb modified

--- termostat.rb
+++ termostat.rb
@@ -1,33 +1,33 @@
 class Termostat
 attr_accessor :set_temperature
 
-	def initialize
-		@set_temperature = @temperature = 20
-	end
+  def initialize
+    @set_temperature = @temperature = 20
+  end
 
-	def check_temperature
-  	@temperature
-  	# define input of measured temperature
+  def check_temperature
+    @temperature
+    # define input of measured temperature
   end
 
   def heating?
-    if @set_temperature > @temperature 
-    	return true
-    	start_heating
-    else 
-    	return false
-    	stop_heating
-  	end
-	end
+    if @set_temperature > @temperature
+      return true
+      start_heating
+    else
+      return false
+      stop_heating
+    end
+  end
 
   private
 
-  def start_heating
-    # define output for heating on
-  end
+    def start_heating
+      # define output for heating on
+    end
 
-	def stop_heating
-	  # define output for heating off
-  end
+    def stop_heating
+      # define output for heating off
+    end
 
 end
